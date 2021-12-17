076111886b96bcd6215b8db09d29113060fb89f4
Tomáš Vojtášek <vojtasek.tomas@seznam.cz>
2021-12-17 14:04:55 +0100

    use old version of wla-dx again

--- .github/scripts/install_wla-dx.sh
+++ .github/scripts/install_wla-dx.sh
@@ -4,5 +4,6 @@ set -euo pipefail
 curl -sSL "https://github.com/${WLA_DX_REPO}/archive/${WLA_DX_COMMIT}.tar.gz" | tar xzv -C "${HOME}"
 mv "${HOME}/wla-dx-${WLA_DX_COMMIT}" "${HOME}/wla-dx"
 cd "${HOME}/wla-dx"
-cmake .
-make
+# cmake .
+# make
+bash ./unix.sh 3 #jobs
